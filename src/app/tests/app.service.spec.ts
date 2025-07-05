f3cf8f5704d6e2ef834bbd7dc57e883f4a858991
peterdinis <petter1@pobox.sk>
2025-07-05 14:18:33 +0200

    feat: Init

---
+++ src/app/tests/app.service.spec.ts
@@ -0,0 +1,13 @@
+import { AppService } from "../app.service";
+
+describe('AppService', () => {
+  let appService: AppService;
+
+  beforeEach(() => {
+    appService = new AppService();
+  });
+
+  it('should return "Hello World!"', () => {
+    expect(appService.getHello()).toBe('Hello World!');
+  });
+});
