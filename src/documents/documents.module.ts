952bfc9808171adcee8c007b0b02c44750a3d4af
peterdinis <petter1@pobox.sk>
2025-07-06 14:21:40 +0200

    fix: Fixing imports in some tests files

--- src/documents/documents.module.ts
+++ src/documents/documents.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import PrismaModule from 'src/prisma/prisma.module';
 import { DocumentController } from './documents.controller';
 import { DocumentService } from './documents.service';
-import { DocumentGateway } from './documents.gateway';
 
 @Module({
   imports: [PrismaModule],
   controllers: [DocumentController],
-  providers: [DocumentService, DocumentGateway],
+  providers: [DocumentService],
 })
 export class DocumentModule {}
