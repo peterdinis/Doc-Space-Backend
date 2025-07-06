e15aac1c3fe3f57876d3d8632cea62a76b495b0b
peterdinis <petter1@pobox.sk>
2025-07-06 17:13:27 +0200

    feat: Updating issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { DocumentModule } from 'src/documents/documents.module';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthModule } from 'src/auth/auth.module';
 import { UserModule } from 'src/user/user.module';
-
+import { SharedDocumentsModule } from 'src/shared-documents/shared-documents.module';
+import { ConnectionsModule } from 'src/connections/connections.module';
+import { MailModule } from 'src/mail/mail.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UserModule, DocumentModule],
+  imports: [PrismaModule, AuthModule, UserModule, DocumentModule, MailModule, ConnectionsModule, SharedDocumentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
 ;
