01f7254b5e3a091ac2fce253841e1c14fb6dfb70
peterdinis <petter1@pobox.sk>
2025-07-06 17:08:19 +0200

    feat: Shared Documents module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import PrismaModule from 'src/prisma/prisma.module';
 import { MailModule } from 'src/mail/mail.module';
 import { ConnectionsModule } from 'src/connections/connections.module';
+import { SharedDocumentsModule } from 'src/shared-documents/shared-documents.module';
 
 @Module({
-  imports: [PrismaModule, MailModule, ConnectionsModule],
+  imports: [PrismaModule, MailModule, ConnectionsModule, SharedDocumentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
