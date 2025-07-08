6c7feb44a919ae154c85147becc016a594fb3f96
peterdinis <petter1@pobox.sk>
2025-07-08 13:05:00 +0200

    feat: Working on connections module

--- src/connections/connections.service.ts
+++ src/connections/connections.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { CreateConnectionDto } from './dto/create-connection.dto';
+import { UpdateConnectionDto } from './dto/update-connection.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class ConnectionService {}
+export class ConnectionsService {
+  constructor(private prisma: PrismaService) {}
+
+  create(requesterId: string, dto: CreateConnectionDto) {
+    return this.prisma.connection.create({
+      data: {
+        requesterId,
+        receiverId: dto.receiverId,
+      },
+    });
+  }
+
+  findUserConnections(userId: string) {
+    return this.prisma.connection.findMany({
+      where: {
+        OR: [
+          { requesterId: userId },
+          { receiverId: userId }
+        ]
+      },
+      include: {
+        requester: true,
+        receiver: true,
+      }
+    });
+  }
+
+  async updateStatus(id: string, dto: UpdateConnectionDto) {
+    return this.prisma.connection.update({
+      where: { id },
+      data: { status: dto.status },
+    });
+  }
+}
