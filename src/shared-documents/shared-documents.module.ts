8c0c84e8e2f568cfd2e8a936ce9ceefe811ab535
peterdinis <petter1@pobox.sk>
2025-07-08 09:16:00 +0200

    feat: Update shared-document.service

--- src/shared-documents/shared-documents.module.ts
+++ src/shared-documents/shared-documents.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { SharedDocumentsService } from './shared-documents.service';
 import { SharedDocumentsController } from './shared-couments.controller';
+import { MailModule } from 'src/mail/mail.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, MailModule],
   providers: [SharedDocumentsService],
   controllers: [SharedDocumentsController],
 })
