4afbd9f0589db15ca36d36c987bce17945c3c6be
peterdinis <petter1@pobox.sk>
2025-07-13 11:54:12 +0200

    fix: Add missing things to providers in connections module

--- src/connections/connections.module.ts
+++ src/connections/connections.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConnectionsController } from './connections.controller';
 import { ConnectionsService } from './connections.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { ConnectionsGateway } from './connections.gateway';
 
 @Module({
   imports: [PrismaModule],
   controllers: [ConnectionsController],
-  providers: [ConnectionsService],
+  providers: [ConnectionsService, ConnectionsGateway],
 })
 export class ConnectionsModule {}
