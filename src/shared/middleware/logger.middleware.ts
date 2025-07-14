625f7c2141a5c64a9d0f2063b09dda16b0e2301f
peterdinis <petter1@pobox.sk>
2025-07-14 18:02:19 +0200

    feat: New logger middleware

---
+++ src/shared/middleware/logger.middleware.ts
@@ -0,0 +1,20 @@
+// src/common/middleware/logger.middleware.ts
+
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    const { method, originalUrl } = req;
+    const startTime = Date.now();
+
+    res.on('finish', () => {
+      const { statusCode } = res;
+      const duration = Date.now() - startTime;
+      console.log(`[${method}] ${originalUrl} -> ${statusCode} (${duration}ms)`);
+    });
+
+    next();
+  }
+}
