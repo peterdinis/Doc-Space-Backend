604904635dfc02768c7f359c77bc4f417bde0c3c
peterdinis <petter1@pobox.sk>
2025-08-05 19:21:43 +0200

    fix: Fixing issue in settings.py with cors

--- core/settings.py
+++ core/settings.py
@@ -56,9 +56,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = ["*"] 
 
 ROOT_URLCONF = 'core.urls'
 
