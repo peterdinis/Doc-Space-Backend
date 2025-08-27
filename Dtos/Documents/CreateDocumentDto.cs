f5f32a47d0809e82f0add7dfc77061bb58222f84
peterdinis <petter1@pobox.sk>
2025-08-27 16:02:18 +0200

    feat: Documents dtos WIP

---
+++ Dtos/Documents/CreateDocumentDto.cs
@@ -0,0 +1,15 @@
+namespace backend.DTOs.Documents
+{
+    public class DocumentCreateDto
+    {
+        [Required]
+        [MaxLength(200)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        public int OwnerId { get; set; }
+
+        [Required]
+        public DocumentContentDto Content { get; set; } = new();
+    }
+}
---
+++ Dtos/Documents/DocumentQueryDto.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace backend.DTOs.Documents
+{
+    public class DocumentQueryDto
+    {
+        public int? OwnerId { get; set; }
+        public string? Search { get; set; }
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+}
