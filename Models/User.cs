2ce305cb8034894ccd94ac75ced53c726d001438
peterdinis <petter1@pobox.sk>
2025-08-28 17:11:35 +0200

    feat: Start to working on auth WIP

---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+public class User
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public required string Username { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public required string Email { get; set; }
+
+    [Required]
+    public required string PasswordHash { get; set; }
+}
